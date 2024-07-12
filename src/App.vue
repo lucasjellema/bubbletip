882ca9a352df6519622c2ceb126668eac84a5cd8
Lucas Jellema <lucasjellema@gmail.com>
2024-07-12 17:31:03 +0200

    Initialize application from par query parameter in URL (that connects to a bucket on OCI Object Storage)

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <v-toolbar-title   @click="gotoIntro()">Bubble Tip</v-toolbar-title>
+      <v-toolbar-title @click="gotoIntro()">Bubble Tip</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn prepend-icon="mdi-star-plus-outline" text to="/newtip">Nieuwe Tip</v-btn>
-      <v-btn prepend-icon="mdi-history"  text to="/recent">Laatste Ontwikkelingen</v-btn>
+      <v-btn prepend-icon="mdi-history" text to="/recent">Laatste Ontwikkelingen</v-btn>
       <v-btn prepend-icon="mdi-book-search-outline" text to="/browse">Zoek en Blader</v-btn>
       <v-btn icon="mdi-map-search" to="/map" title="Bekijk en zoek tips op kaart"></v-btn>
       <v-btn text to="/request">Vraag om Tip</v-btn>
@@ -19,9 +19,28 @@
 </template>
 
 <script setup>
-  import router from './router'
-  //
-  const gotoIntro = () => {
-    router.push('/')
+
+import router from './router'
+import { useAppStore } from "@/stores/app";
+const appStore = useAppStore()
+
+const initializeStore = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has('par')) {
+    // http://<host:port/apppath>/?par=https://objectstorage.us-ashburn-1.oraclecloud.com/p/3ZvD2n18VN6y/n/idtwlqf2hanz/b/website/o/
+    const bucketPAR = urlParams.get('par')
+    appStore.setPAR(bucketPAR)
   }
+}
+
+onMounted(() => {
+  console.log('***** App Mounted!!')
+  initializeStore()
+})
+//
+
+const gotoIntro = () => {
+  router.push('/')
+}
+
 </script>
