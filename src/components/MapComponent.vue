fb0e0c92be119c80a99cdf2777b6db7b575bb243
Lucas Jellema <lucasjellema@gmail.com>
2024-07-13 19:33:20 +0200

    Add Leaflet; Add MapComponent in Tip editor; leverage reverse geocoding

---
+++ src/components/MapComponent.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <p>Klik op de kaart om de locatie aan te geven van jouw tip. Je kunt de marker verplaatsen of een nieuwe
+            plaatsen.</p>
+        <div id="map" style="height: 500px;"></div>
+        <p>Je kunt de adresgegevens voor de tip opvragen als je de marker hebt geplaatst.</p>
+    </div>
+</template>
+<script setup>
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+const model = defineModel();
+const emit = defineEmits(['update:coordinates']);
+const props = defineProps({
+    initialCoordinates: {
+        type: Object,
+        default: {
+            lat: 51.505,
+            lng: -0.09
+        }
+    },
+    label: {
+        type: String,
+        default: 'Klik op de kaart om de locatie te bepalen'
+    }
+})
+onMounted(() => {
+    initMap();
+})
+let map, marker
+
+watch(() => props.label
+    , (newValue, oldValue) =>
+        marker.setPopupContent(newValue)
+)
+
+const initMap = () => {
+    // Initialize the map
+    map = L.map('map').setView([props.initialCoordinates.lat, props.initialCoordinates.lng], 13);
+
+    // Set up the OSM layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+    }).addTo(map);
+
+    // Add initial marker if coordinates are provided
+    marker = L.marker([props.initialCoordinates.lat, props.initialCoordinates.lng], {
+        draggable: true
+    }).addTo(map).bindPopup(props.label).openPopup();
+
+    // Event listener for marker drag
+    marker.on('dragend', onMarkerDrag);
+
+    // Event listener for map click
+    map.on('click', onMapClick);
+}
+const onMarkerDrag = (event) => {
+    const position = event.target.getLatLng();
+    emit('update:coordinates', { lat: position.lat, lng: position.lng });
+}
+
+const onMapClick = (event) => {
+    if (marker) {
+        map.removeLayer(marker);
+    }
+    marker = L.marker([event.latlng.lat, event.latlng.lng], {
+        draggable: true
+    }).addTo(map).bindPopup(props.label).openPopup();
+
+    // Event listener for marker drag
+    marker.on('dragend', onMarkerDrag);
+
+    emit('update:coordinates', { lat: event.latlng.lat, lng: event.latlng.lng });
+}
+</script>
+
+
+<style scoped>
+#map {
+    width: 100%;
+    height: 100%;
+}
+</style>
--- src/pages/newtip.vue
+++ src/pages/newtip.vue
@@ -2,70 +2,130 @@
   <h1>Nieuwe Tip</h1>
   <v-container class="mb-0 mt-1" fluid>
     <v-row>
-      <v-col class="py-2" cols="12">
-        <p>Type Tip</p>
-        <v-btn-toggle v-model="tipType" mandatory color="deep-purple-accent-3">
-          <v-btn value="verblijf" title="Overnachtingsplek - hotel, camping, appartement, ...">
-            <v-icon start>
-              mdi-bed
-            </v-icon>
-            <span class="hidden-sm-and-down">Overnachtingsplek</span>
-          </v-btn>
+      <v-col class="py-2" cols="6">
 
-          <v-btn value="restaurant" title="Plek om te eten - restaurant, cafe, pizzeria, bakkerij, ...">
-            <v-icon start>
-              mdi-silverware-fork-knife
-            </v-icon>
-            <span class="hidden-sm-and-down">Restaurant</span>
-          </v-btn>
+        <v-container class="mb-0 mt-1" fluid>
+          <v-row>
+            <v-col class="py-2" cols="12">
+              <p>Type Tip</p>
+              <v-btn-toggle v-model="tipType" mandatory color="deep-purple-accent-3">
+                <v-btn value="verblijf" title="Overnachtingsplek - hotel, camping, appartement, ...">
+                  <v-icon start>
+                    mdi-bed
+                  </v-icon>
+                  <span class="hidden-sm-and-down">Overnachtingsplek</span>
+                </v-btn>
 
-          <v-btn value="activiteit">
-            <v-icon start>
-              mdi-walk
-            </v-icon>
-            <span class="hidden-sm-and-down">Activiteit</span>
-          </v-btn>
+                <v-btn value="restaurant" title="Plek om te eten - restaurant, cafe, pizzeria, bakkerij, ...">
+                  <v-icon start>
+                    mdi-silverware-fork-knife
+                  </v-icon>
+                  <span class="hidden-sm-and-down">Restaurant</span>
+                </v-btn>
 
-        </v-btn-toggle>
+                <v-btn value="activiteit">
+                  <v-icon start>
+                    mdi-walk
+                  </v-icon>
+                  <span class="hidden-sm-and-down">Activiteit</span>
+                </v-btn>
+
+              </v-btn-toggle>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="py-2" cols="11">
+              <v-text-field label="Naam" v-model="naam"
+                hint="Naam van hotel, camping, restaurant, museum, attractie"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="py-2" cols="11">
+              <v-expansion-panels :multiple="true">
+                <v-expansion-panel title="Plek" collapse-icon="mdi-map-marker" expand-icon="mdi-map-marker">
+                  <v-expansion-panel-text>
+                    <v-container class="mb-0 mt-1" fluid>
+                      <v-row>
+                        <v-col class="py-2" cols="10">
+                          <v-text-field label="Adres" v-model="adresgegevens"
+                            hint="Straat, huisnummer, postcode, stad, land"></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="4">
+                          <v-text-field label="Straat" v-model="straat"></v-text-field>
+                        </v-col>
+                        <v-col cols="2">
+                          <v-text-field label="Nummer" v-model="huisnummer"></v-text-field>
+                        </v-col>
+                        <v-col cols="3">
+                          <v-text-field label="Wijk" v-model="wijk"></v-text-field>
+                        </v-col>
+                      </v-row>
+                      <v-row>
+                        <v-col cols="2">
+                          <v-text-field label="Postcode" v-model="postcode"></v-text-field>
+                        </v-col>
+                        <v-col cols="3">
+                          <v-text-field label="Stad" v-model="stad"></v-text-field>
+                        </v-col>
+                        <v-col cols="4">
+                          <v-text-field label="Land" v-model="land"></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-expansion-panel-text>
+                </v-expansion-panel>
+              </v-expansion-panels>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="py-2" cols="11">
+              <v-text-field label="Website" v-model="website"
+                hint="website (url) van hotel, restaurant, museum, attractie"></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="py-2" cols="11">
+              <p>Beschrijving (van hotel, restaurant, museum, attractie: wat is het? waarom is het leuk? hoe kom je er?
+                wat kost het?)</p>
+              <v-sheet class="flex-1-1-100  ma-0 pa-0 mb-3">
+                <QuillEditor theme="snow" toolbar="essential" v-model:content="beschrijving" contentType="delta" />
+              </v-sheet>
+            </v-col>
+          </v-row>
+        </v-container>
       </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="py-2" cols="8">
-        <v-text-field label="Naam" v-model="naam" hint="Naam van hotel, camping, restaurant, museum, attractie"></v-text-field>
+
+      <v-col class="py-2" cols="6">
+
+        <MapComponent :initialCoordinates="{ lat: 51.505, lng: 5.09 }" :label="naam" v-model="geocoordinates"
+          @update:coordinates="updateCoordinates" />
+
+        <v-btn @click="refreshLocationDetailsThroughGeocoder()"
+          title="Gebruik geodecoding om adresgegevens op te halen op basis van geocoordinaten"
+          v-if="geocoordinates != null && geocoordinates.lat && geocoordinates.lng">
+          <v-icon start>
+            mdi-map-marker-down
+          </v-icon>
+          <span class="hidden-sm-and-down">Haal adresgegevens op op basis van geocoordinaten</span>
+        </v-btn>
+
       </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="py-2" cols="8">
-        <v-text-field label="Adres" v-model="adresgegevens"
-          hint="Straat, huisnummer, postcode, stad, land"></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="py-2" cols="8">
-        <v-text-field label="Website" v-model="website"
-          hint="website (url) van hotel, restaurant, museum, attractie"></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="py-2" cols="8">
-        <p>Beschrijving (van hotel, restaurant, museum, attractie: wat is het? waarom is het leuk? hoe kom je er? wat kost het?)</p>
-        <v-sheet class="flex-1-1-100  ma-0 pa-0 mb-3">
-          <QuillEditor theme="snow" toolbar="essential" v-model:content="beschrijving" contentType="delta" />
-        </v-sheet>
-        <!-- <v-textarea label="Beschrijving" v-model="beschrijving" hint="beschrijving van hotel, restaurant, museum, attractie: wat is het? waarom is het leuk? hoe kom je er? wat kost het?"></v-textarea> -->
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-btn title="Sla tip op" class="mb-2" @click="saveTip()">Sla Tip Op</v-btn>
     </v-row>
   </v-container>
+  <v-btn title="Sla tip op" class="mb-2" @click="saveTip()">Sla Tip Op</v-btn>
 </template>
 
 <script setup>
+import { useGeoLibrary } from '@/composables/useGeoLibrary';
+const { reverseGeocode } = useGeoLibrary();
+
 import { useAppStore } from "@/stores/app";
 import { onBeforeMount, onMounted } from "vue";
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import MapComponent from '@/components/MapComponent.vue';
 
 const appStore = useAppStore()
 const bubble = appStore.getBubble()
@@ -76,6 +136,13 @@ const adresgegevens = ref('')
 const website = ref('')
 const beschrijving = ref('') // rich text
 const geocoordinates = ref([])
+const stad = ref('')
+const land = ref('')
+const straat = ref('')
+const postcode = ref('')
+const wijk = ref('')
+const huisnummer = ref('')
+
 const tags = ref([])
 // oorspronkelijk ikookje
 const wanneer = ref(null)
@@ -85,6 +152,28 @@ const beoordeling = ref(1) // 0 = ok/goed, 1= heel/erg goed, 2 = super/topperdet
 // fotos
 
 //
+
+const updateCoordinates = (newCoordinates) => {
+  geocoordinates.value = newCoordinates;
+}
+
+const refreshLocationDetailsThroughGeocoder = async () => {
+  const coordinates = geocoordinates.value
+  if (coordinates != null && coordinates.lat && coordinates.lng) {
+    const locationDetails = await reverseGeocode(coordinates.lng, coordinates.lat)
+    adresgegevens.value = locationDetails.address
+    naam.value = locationDetails.label
+    land.value = locationDetails.country
+    straat.value = locationDetails.street
+    stad.value = locationDetails.city
+    postcode.value = locationDetails.postcode
+    wijk.value = locationDetails.quarter
+    huisnummer.value = locationDetails.house_number
+  }
+
+
+  console.log('locationDetails', locationDetails)
+}
 
 const saveTip = () => {
   const tip = {
